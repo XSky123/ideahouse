e617abab2b1ca060f15eb62f7d7989f96df646c7
xsky123 <dlxiaotian01@gmail.com>
2015-01-29 17:38:15 +0800

    footer indepedent

--- gather/index.php
+++ gather/index.php
@@ -121,7 +121,7 @@
 							<?php 
 								}
 							 ?>
-									<td>
+									<td width="15%" align="center">
 										<h4><a href=<?php echo $row['addr']; ?>><?php echo $row['site']; ?></a></h4>
 									</td>
 							<?php
@@ -285,6 +285,7 @@
 		</div>
 		</div>
 	</div>
+	<?php include_once("footer.php") ?>
 	<footer>
 		<p class="pull-right"><a href="#">Back to top</a></p>
 		<p>&copy; 2014 XSky123 &middot; <a href="../index.php">晓天的灵感屋</a></p>
