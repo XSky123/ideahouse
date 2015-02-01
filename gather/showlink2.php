c108e1d84cdebf23b85d7aeeecbe3a51bcff2fac
xsky123 <dlxiaotian01@gmail.com>
2015-02-01 21:14:28 +0800

    Smaller Fontsize

--- gather/showlink2.php
+++ gather/showlink2.php
@@ -13,7 +13,7 @@ if (!$row['is_recommend']){
 	}
  ?>
 		<td width="15%" align="center">
-			<h4><a href=<?php echo "http://".$row['addr']; ?>><?php echo $row['site']; ?></a></h4>
+			<h5><a href=<?php echo "http://".$row['addr']; ?>><?php echo $row['site']; ?></a></h5>
 		</td>
 <?php
 	if($row['type2']!=$currentType){
